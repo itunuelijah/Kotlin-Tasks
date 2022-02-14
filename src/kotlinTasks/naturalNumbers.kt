9c8e4fbb2ef016b6ffa62a2fe8d1f106ab7816b6
itunuelijah <itunuelijah@gmail.com>
2022-02-14 15:44:56 +0100

    natural number

---
+++ src/kotlinTasks/naturalNumbers.kt
@@ -0,0 +1,19 @@
+fun main(){
+
+    var total = 0;
+    var count = 1
+
+    while(count <= 100){
+
+        total = total + count
+
+        println("loop : $count")
+        count++
+    }
+
+    println("this is the sum " + total)
+}
+
+
+
+
