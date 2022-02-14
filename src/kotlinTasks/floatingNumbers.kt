cf13da75d655cb11b12066e174bf60814f424bce
itunuelijah <itunuelijah@gmail.com>
2022-02-14 15:42:37 +0100

    floating numbers

---
+++ src/kotlinTasks/floatingNumbers.kt
@@ -0,0 +1,17 @@
+import java.util.Scanner
+
+fun main(){
+
+    var reader = Scanner(System.`in`)
+
+    println("enter number")
+    var number1 = reader.nextFloat()
+
+    println("enter number2")
+    var number2 = reader.nextFloat()
+
+    var result = number1 * number2
+
+    println(result)
+
+}
